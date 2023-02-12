98b5e78ad012b22ed5cf5baffd0ee1f424c44de6
samantrs <https://samantrs@git.rabobank.nl>
2023-02-13 00:31:13 +0100

    configuring web client

--- web-client/src/main/java/com/configure/webclient/config/WebClientConfig.java
+++ web-client/src/main/java/com/configure/webclient/config/WebClientConfig.java
@@ -2,6 +2,7 @@ package com.configure.webclient.config;
 
 import com.configure.webclient.properties.ClientConnectionProperties;
 import io.netty.channel.ChannelOption;
+import io.netty.channel.epoll.EpollChannelOption;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
@@ -13,8 +14,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
 
 import java.time.Duration;
 
@@ -23,6 +26,13 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 @Slf4j
 @Configuration
 public class WebClientConfig {
+
+    public static final int MAX_BUFFER_SIZE_BYTES = 2 * 1024 * 1024; // 2 mb
+    private static final int ACQUIRE_TIMEOUT_SECONDS = 5;
+    private static final int MAX_CONNECTIONS = 2000;
+    private static final int TCP_KEEPIDLE_SECONDS = 3000;
+    private static final int TCP_KEEPINTVL_SECONDS = 60;
+    private static final int TCP_KEEPCNT = 8;
 
     @Bean
     public WebClient getEmployeeWebClient(@Qualifier("getEmployeeHttpClient") HttpClient httpClient,
@@ -35,6 +45,10 @@ public class WebClientConfig {
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .filter(requestFilter)
                 .filter(responseFilter)
+                .exchangeStrategies(ExchangeStrategies
+                        .builder()
+                        .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(MAX_BUFFER_SIZE_BYTES))
+                        .build())
                 .build();
     }
 
@@ -49,37 +63,60 @@ public class WebClientConfig {
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .filter(requestFilter)
                 .filter(responseFilter)
+                .exchangeStrategies(ExchangeStrategies
+                        .builder()
+                        .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(MAX_BUFFER_SIZE_BYTES))
+                        .build())
                 .build();
     }
 
     @Bean
     public HttpClient getEmployeeHttpClient(SslContext sslContext,
                                  @Qualifier("getEmployeeConnectionProperties") ClientConnectionProperties connectionProperties) {
-        return HttpClient.create()
-                .wiretap(true)
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, connectionProperties.getConnectionTimeout())
-                .doOnConnected(connection ->
-                        connection.addHandlerLast(new ReadTimeoutHandler(connectionProperties.getReadTimeout(), MILLISECONDS))
-                                .addHandlerLast(new WriteTimeoutHandler(connectionProperties.getWriteTimeout(), MILLISECONDS)))
-                .responseTimeout(Duration.ofSeconds(2))
-                .secure(sslSpec ->
-                        sslSpec.sslContext(sslContext)
-                );
+
+        ConnectionProvider getHttpClientConnectionProvider = ConnectionProvider
+                .builder("get-webclient-connection-pool")
+                .maxConnections(MAX_CONNECTIONS)
+                .pendingAcquireTimeout(Duration.ofSeconds(ACQUIRE_TIMEOUT_SECONDS))
+                .pendingAcquireMaxCount(-1)
+                .build();
+
+        return HttpClient
+            .create(getHttpClientConnectionProvider)
+            .secure(sslSpec -> sslSpec.sslContext(sslContext))
+            .wiretap(true)
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, connectionProperties.getConnectionTimeout())
+            .option(ChannelOption.SO_KEEPALIVE, true)
+            .option(EpollChannelOption.TCP_KEEPIDLE, TCP_KEEPIDLE_SECONDS)
+            .option(EpollChannelOption.TCP_KEEPINTVL, TCP_KEEPINTVL_SECONDS)
+            .option(EpollChannelOption.TCP_KEEPCNT, TCP_KEEPCNT)
+            .doOnConnected(connection ->
+                    connection.addHandlerLast(new ReadTimeoutHandler(connectionProperties.getReadTimeout(), MILLISECONDS))
+                            .addHandlerLast(new WriteTimeoutHandler(connectionProperties.getWriteTimeout(), MILLISECONDS)));
     }
 
     @Bean
     public HttpClient postEmployeeHttpClient(SslContext sslContext,
                                  @Qualifier("postEmployeeConnectionProperties") ClientConnectionProperties connectionProperties) {
-        return HttpClient.create()
+
+        ConnectionProvider postHttpClientConnectionProvider = ConnectionProvider
+                .builder("post-webclient-connection-pool")
+                .maxConnections(MAX_CONNECTIONS)
+                .pendingAcquireTimeout(Duration.ofSeconds(ACQUIRE_TIMEOUT_SECONDS))
+                .pendingAcquireMaxCount(-1)
+                .build();
+
+        return HttpClient
+                .create(postHttpClientConnectionProvider)
+                .secure(sslSpec -> sslSpec.sslContext(sslContext))
                 .wiretap(true)
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, connectionProperties.getConnectionTimeout())
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .option(EpollChannelOption.TCP_KEEPIDLE, TCP_KEEPIDLE_SECONDS)
+                .option(EpollChannelOption.TCP_KEEPINTVL, TCP_KEEPINTVL_SECONDS)
+                .option(EpollChannelOption.TCP_KEEPCNT, TCP_KEEPCNT)
                 .doOnConnected(connection ->
                         connection.addHandlerLast(new ReadTimeoutHandler(connectionProperties.getReadTimeout(), MILLISECONDS))
-                                .addHandlerLast(new WriteTimeoutHandler(connectionProperties.getWriteTimeout(), MILLISECONDS)))
-                .responseTimeout(Duration.ofSeconds(2))
-                .secure(sslSpec ->
-                        sslSpec.sslContext(sslContext)
-                );
+                                .addHandlerLast(new WriteTimeoutHandler(connectionProperties.getWriteTimeout(), MILLISECONDS)));
     }
-
 }
